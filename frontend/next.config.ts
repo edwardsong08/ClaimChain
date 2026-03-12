0cff833d40db9013ee821e3cb69254a2f51a3a10
Edward Song <111555193+edwardsong08@users.noreply.github.com>
2026-03-11 20:06:41 -0400

    deployment fix: https and http are incompatible
    - change frontend destination from http.//18.222.221.138 to /backend-api/

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async rewrites() {
+    return [
+      {
+        source: "/backend-api/:path*",
+        destination: "http://18.222.221.138/:path*",
+      },
+    ];
+  },
 };
 
 export default nextConfig;
